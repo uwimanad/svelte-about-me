0c8128f1abd547c76145d54f0f18aa1afcddcb0c
Divine Uwimana <du1@williams.edu>
2025-07-17 16:49:58 -0500

    updating pictures

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,50 @@
   let favoriteNumber = 7; // Change this to your favorite number
   let bio = "I am a web developer with a passion for DATA science";
   let hobbies = ["Coding", "Badminton", "Baking"]; // List some hobbies
+  import ImageCard from "./lib/ImageCard.svelte";
+
+  let cardData = [
+    {
+      image:
+        "https://media.istockphoto.com/id/1448152453/vector/big-data-technology-and-data-science-illustration-data-flow-concept-querying-analysing.jpg?s=612x612&w=0&k=20&c=To0lhCrVmDYdSkOUOGxGsjlYe0buj_wwGCDqYhF9p2o=",
+      title: "Coding",
+      description:
+        "I love solving problems and writing code to build cool things.",
+    },
+    {
+      image:
+        "https://mekreview.com/wp-content/uploads/2021/03/clay-banks-w_qTfiPbjbg-unsplash-scaled.jpg",
+      title: "Reading",
+      description: "Books help me grow intellectually and creatively.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1597528662465-55ece5734101?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFraW5nfGVufDB8fDB8fHww",
+      title: "Baking",
+      description: "I enjoy making cakes and cookiess",
+    },
+  ];
 </script>
+
+<div class="container">
+  <h1>About Me</h1>
+  <p>Hello, my name is <strong>{name}</strong>!</p>
+  <p>My favorite number is <strong>{favoriteNumber}</strong>.</p>
+  <p>{bio}</p>
+
+  <h2>My Hobbies</h2>
+  <h2>Some of My Favorite Things</h2>
+  <div class="card-container">
+    {#each cardData as { image, title, description }}
+      <ImageCard {image} {title} {description} />
+    {/each}
+  </div>
+  <div class="hobbies">
+    {#each hobbies as hobby}
+      <div class="hobby">{hobby}</div>
+    {/each}
+  </div>
+</div>
 
 <style>
   h1 {
@@ -29,46 +72,3 @@
     border-radius: 5px;
   }
 </style>
-
-<div class="container">
-  <h1>About Me</h1>
-  <p>Hello, my name is <strong>{name}</strong>!</p>
-  <p>My favorite number is <strong>{favoriteNumber}</strong>.</p>
-  <p>{bio}</p>
-
-  <h2>My Hobbies</h2>
-    <h2>Some of My Favorite Things</h2>
-  <div class="card-container">
-    {#each cardData as { image, title, description }}
-      <ImageCard {image} {title} {description} />
-    {/each}
-  </div>
-  <div class="hobbies">
-    {#each hobbies as hobby}
-      <div class="hobby">{hobby}</div>
-    {/each}
-  </div>
-</div>
-
-  import ImageCard from "./lib/ImageCard.svelte";
-  
-  let cardData = [
-    {
-      image:
-        "https://media.istockphoto.com/id/185590965/photo/yellow-rubber-duck-for-bath-time.jpg?s=612x612&w=0&k=20&c=QoT-O5jbOugCgdQhLat15c0L9jCmRrSTiO9U50W_eQc=",
-      title: "Coding",
-      description:
-        "I love solving problems and writing code to build cool things.",
-    },
-    {
-      image: "https://media.istockphoto.com/id/1460007178/photo/library-books-on-table-and-background-for-studying-learning-and-research-in-education-school.jpg?s=612x612&w=is&k=20&c=PvjSrEGbw3G64q74ccIQaJ9nEj9AzPwjvRo1cWFA3UU=",
-      title: "Reading",
-      description: "Books help me grow intellectually and creatively.",
-    },
-    {
-      image: "https://media.istockphoto.com/id/1440399157/photo/international-airport-terminal-asian-beautiful-woman-with-luggage-and-walking-in-airport.jpg?s=612x612&w=0&k=20&c=RqmI0KG_pnEb_GmWLC6pY5MoV75JBt1nU1L9dStkJys=",
-      title: "Traveling",
-      description:
-        "I enjoy exploring new cultures and learning from the world around me.",
-    },
-  ];
